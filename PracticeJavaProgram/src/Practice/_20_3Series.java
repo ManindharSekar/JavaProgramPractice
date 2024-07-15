0eb93332259e246d1e49f038c324ae8c1fe62ecb
bzf <bzf@DESKTOP-3ENAFVQ>
2024-07-15 12:21:11 +0530

    commit java programs

---
+++ PracticeJavaProgram/src/Practice/_20_3Series.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class _20_3Series {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int a=0;
+		for(int i=0;i<n;i++) {
+			a=(a*10)+3;
+			System.out.print(a+" ");
+		}
+
+	}
+
+}
