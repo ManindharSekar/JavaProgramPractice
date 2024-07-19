bef930967105e8a173215834374d2720bc0e1018
bzf <bzf@DESKTOP-3ENAFVQ>
2024-07-19 17:07:42 +0530

    commit java program

---
+++ PracticeJavaProgram/src/Practice/_47_Lpattern9_0.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class _47_Lpattern9_0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(a);
+				a--;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
