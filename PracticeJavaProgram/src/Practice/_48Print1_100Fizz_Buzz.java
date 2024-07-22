78ec3b96cc3771434b914df1f5db98b2ba558eda
bzf <bzf@DESKTOP-3ENAFVQ>
2024-07-22 09:34:43 +0530

    commit java program

---
+++ PracticeJavaProgram/src/Practice/_48Print1_100Fizz_Buzz.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class _48Print1_100Fizz_Buzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=100;
+		for(int i=1;i<=n;i++) {
+			if(i%3==0&i%5==0) {
+				System.out.print("FizzBuzz ");
+			}else if(i%3==0) {
+				System.out.print("Fizz ");
+			}else if(i%5==0) {
+				System.out.print("Buzz ");
+			}else {
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
